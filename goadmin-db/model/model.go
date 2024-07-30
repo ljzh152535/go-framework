1c362e55b9a70f8846a9490a746a6f37cc2f85ff
fengfeng <ljzh152535@163.com>
2024-07-30 09:35:50 +0800

    修改GVA_MODEl

--- goadmin-db/model/model.go
+++ goadmin-db/model/model.go
@@ -35,7 +35,7 @@ type DBItem struct {
 
 type GVA_MODEL struct {
 	ID        uint           `json:"id" gorm:"primarykey" form:"id"` // 主键ID
-	CreatedAt time.Time      // 创建时间
-	UpdatedAt time.Time      // 更新时间
-	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"` // 删除时间
+	CreatedAt time.Time      `json:"createdAt"`                      // 创建时间
+	UpdatedAt time.Time      `json:"updatedAt"`                      // 更新时间
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`                 // 删除时间
 }
